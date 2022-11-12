9771b90188b3d700dc03a92b924c79cacfd73e56
roshan <roshanprusty100@gmail.com>
2022-11-12 17:56:07 +0530

    Version22

--- Resume_builder/scriptrb.js
+++ Resume_builder/scriptrb.js
@@ -1,37 +1,37 @@
-$(document).ready(function() {
-  var $element1 = $('#dot1');
-  var $element2 = $("#dot2");
-  var $element3 = $("#dot3");
-  var $element4 = $("#dot4");
+// $(document).ready(function() {
+//   var $element1 = $('#dot1');
+//   var $element2 = $("#dot2");
+//   var $element3 = $("#dot3");
+//   var $element4 = $("#dot4");
 
-  function fadeInOut() {
+//   function fadeInOut() {
 
-    $element1.fadeIn('fast', function() {
-      $element1.delay(3000).fadeOut('slow', function() {
-        $element1.fadeIn('fast', function() {
-          setTimeout(fadeInOut, 5);
-        });
-      });
-    });
+//     $element1.fadeIn('fast', function() {
+//       $element1.delay(3000).fadeOut('slow', function() {
+//         $element1.fadeIn('fast', function() {
+//           setTimeout(fadeInOut, 5);
+//         });
+//       });
+//     });
 
-    $element2.fadeIn('fast', function() {
-      $element2.delay(2950).fadeOut('slow', function() {
-        $element2.fadeIn('fast', function() {      
-        });
-      });
-    });
+//     $element2.fadeIn('fast', function() {
+//       $element2.delay(2950).fadeOut('slow', function() {
+//         $element2.fadeIn('fast', function() {      
+//         });
+//       });
+//     });
   
 
-  };
+//   };
 
-  fadeInOut();
+//   fadeInOut();
 
-  /* Progress Bar Animation */
-  $(".progress-bar").animate({
-    width: "100%"
-  }, 2500);
+//   /* Progress Bar Animation */
+//   $(".progress-bar").animate({
+//     width: "100%"
+//   }, 2500);
 
-});
+// });
 
 const getState = () => {
   const $ = (element) => {
